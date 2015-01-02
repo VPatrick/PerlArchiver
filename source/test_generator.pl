df4a9573f573afbca8932c87caacf813d81e4939
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2015-01-02 15:50:22 +0100

    reduce amount of folders and files

--- source/test_generator.pl
+++ source/test_generator.pl
@@ -4,14 +4,14 @@ use warnings;
 
 my $dirname = "test";
 
-for (0 .. 999) {
+for (0 .. 4) {
 	$dirname = $dirname . "/test";
 }
 
 my @folders = split /\/|\\/, $dirname;
 map {
 	mkdir $_;
-	for (0 .. 999) {
+	for (0 .. 499) {
 		my $file = "test_" . $_ . ".txt";
 		unless(open FILE, '>'.$file) {
 			next;
