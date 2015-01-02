0a44935fd6fa754daecfc1bcd36921728e3cefd4
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2015-01-02 18:39:46 +0100

    Add new methods

--- source/verbosity.pm
+++ source/verbosity.pm
@@ -29,15 +29,54 @@ sub verbose {
 	}
 };
 
+sub message {
+	my ($self, $message, $state) = @_;
+	if ($state) {
+		print $self->colourise($state), "$message\n";
+	} else {
+		print "$message\n";
+	}
+};
+
+sub ok {
+	use Term::ANSIColor;
+	my ($self, $message) = @_;
+	if ($message) {
+		return color("bold green"), "[OK]\t", color("reset"), "$message\n";
+	} else {
+		return color("bold green"), "[OK]\t", color("reset");
+	}
+};
+
+sub warning {
+	use Term::ANSIColor;
+	my ($self, $message) = @_;
+	if ($message) {
+		return color("bold yellow"), "[WARNING]\t", color("reset"), "$message\n";
+	} else {
+		return color("bold yellow"), "[WARNING]\t", color("reset");
+	}
+};
+
+sub error {
+	use Term::ANSIColor;
+	my ($self, $message) = @_;
+	if ($message) {
+		return color("bold red"), "[ERROR]\t", color("reset"), "$message\n";
+	} else {
+		return color("bold red"), "[ERROR]\t", color("reset");
+	}
+};
+
 sub colourise {
 	use Term::ANSIColor;
 	my ($self, $state) = @_;
 	if ($state eq "OK") {
-		return color("bold green"), "[OK]\t", color("reset");
+		return $self->ok();
 	} elsif ($state eq "WARNING") {
-		return color("bold yellow"), "[WARNING]\t", color("reset");
+		return $self->warning();
 	} elsif ($state eq "ERROR") {
-		return color("bold red"), "[ERROR]\t", color("reset");
+		return $self->error();
 	} else {
 		return "";
 	}
