2cbf56530563475c1684fac839f56d8468cbdf5a
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2015-01-02 19:47:22 +0100

    Comment test generator

--- source/test_generator.pl
+++ source/test_generator.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/perl
+# Author: Patrick Vogt
 use strict;
 use warnings;
 
+# Erstellen des tiefsten Verzeichnisnamens
+# Die Tiefe kann durch die Erhöhung des hinteren Wertes im Kopf der for-Schleife
+#	erhöht werden.
 my $dirname = "test";
-
 for (0 .. 4) {
 	$dirname = $dirname . "/test";
 }
@@ -11,9 +14,12 @@ for (0 .. 4) {
 my @folders = split /\/|\\/, $dirname;
 map {
 	mkdir $_;
+	# Erstellen von Dateien in den erstellten Verzeichnissen
+	# Die Anzahl der Dateien kann durch die Erhöhung des hinteren Wertes im Kopf
+	#    der for-Schleife erhöht werden.
 	for (0 .. 499) {
 		my $file = "test_" . $_ . ".txt";
-		unless(open FILE, '>'.$file) {
+		unless(open FILE, '>' . $file) {
 			next;
 		}
 		print FILE "Test " . $_;
