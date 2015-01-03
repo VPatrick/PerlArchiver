6e35927233857cc2d20bbc187f08a7c8024a0e25
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2015-01-03 01:49:21 +0100

    list

--- source/list.pm
+++ source/list.pm
@@ -1,7 +1,9 @@
 use strict;
 use warnings;
-use Verbosity;
+use Cwd;
 use Message;
+use Utils;
+use Verbosity;
 
 package List;
 
@@ -10,35 +12,54 @@ sub new {
 	my $class = ref($invocant) || $invocant;
 	my $self = {
 		verbosity => Verbosity->new,
-		message => Message->new
+		message => Message->new,
+		utils => Utils->new
 	};
 	bless ($self, $class);
 	return $self;
 };
 
+sub setVerboseLevel {
+	my ($self, $level) = @_;
+	$self->{verbosity}->setVerboseLevel($level);
+};
+
 sub list {
 	use File::Find qw(find);
 	my ($self, $archive, $timestamp) = @_;
-	if ($timestamp !~ m/^\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}/) {
-		print $self->{message}->error("Der Zeitstempel entspricht nicht dem vorgegebenen Format: yyyy_mm_dd_HH_MM_SS");
+	if ($timestamp !~ m/^\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}/i) {
+		print $self->{message}->error("No valid timestamp: yyyy_mm_dd_HH_MM_SS");
 		exit;
+	}
+	
+	chdir $archive;
+	$archive = Cwd::getcwd();
+	
+	if ($self->{verbosity}->getVerboseLevel() > 0) {
+		$self->{utils}->setVerboseLevel($self->{verbosity}->getVerboseLevel());
 	}
 	
 	my @list;
 	find(sub {
 		return if($_ eq '.' || $_ eq '..');
 		push @list, $File::Find::name;
-	}, $archive || ".");
+	}, $self->{utils}->findLastValidArchive($archive, $timestamp));
+	
 	$self->print_list(@list);
 };
 
 sub print_list {
 	my ($self, @list) = @_;
 	foreach (@list) {
-		if (-d $_) {
-			print "d\t$_\n";
+		if (-l $_) {
+			$_ =~ m|(/[^/]+?)$|;
+			print "link\t$1\n";
+		} elsif (-d $_) {
+			$_ =~ m|(/[^/]+?)$|;
+			print "dir\t$1\n";
 		} else {
-			print "\t$_\n";
+			$_ =~m|(/[^/]+?)$|;
+			print "\t$1\n";
 		}
 	}
 };
