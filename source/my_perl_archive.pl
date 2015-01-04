8c2cf978c08445da4e47b44e7caf597d8d2c3e3d
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2015-01-04 11:22:14 +0100

    add help function

--- source/my_perl_archive.pl
+++ source/my_perl_archive.pl
@@ -7,6 +7,8 @@ use Cwd qw(abs_path);
 use lib dirname(dirname abs_path $0) . "/PerlArchiver/source";
 
 use Getopt::Long;
+use Pod::Usage qw(pod2usage);
+use FindBin;
 use Invoker;
 
 my $i = Invoker->new();
@@ -18,5 +20,9 @@ GetOptions (
 	"slim|s" => sub { $i->slim(@ARGV) },
 	"delete|d" => sub { $i->del(@ARGV) },
 	"list|l" => sub { $i->list(@ARGV) },
-	"help|h" => sub { $i->help() }
+	"help|h" => sub { help() }
 ) or die("Error in command line arguments\n");
+
+sub help() {
+	pod2usage(-verbose => 2, -input => $FindBin::Bin . "/help.pod");
+};
