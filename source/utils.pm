6d911a330c366f10de4cb8a235f7067a82b7a23a
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2015-01-06 19:06:11 +0100

    source code documentation

--- source/utils.pm
+++ source/utils.pm
@@ -2,8 +2,13 @@ use strict;
 use warnings;
 use Verbosity;
 
+# Utils
+# Beschreibung: Hilfsklasse
+# Autor:		Patrick Vogt, Muhammed Kasikci
+# Datum:		Januar 2015
 package Utils;
 
+# Konstruktor
 sub new {
 	my ($invocant) = @_;
 	my $class = ref($invocant) || $invocant;
@@ -14,30 +19,32 @@ sub new {
 	return $self;
 };
 
+# setVerboseLevel
+# Beschreibung: Setzt das Verbose-Level
+# Parameter:	level	Verbose-Level
 sub setVerboseLevel {
 	my ($self, $level) = @_;
 	$self->{verbosity}->setVerboseLevel($level);
 };
 
+# findLastValidArchive
+# Beschreibung: Findet das zuletzt gültige Archiv
+# Parameter:	source		Verzeichnis für Archive
+#				timestamp	Zeitpunkt
 sub findLastValidArchive {
 	my ($self, $source, $timestamp) = @_;
-	opendir(my $dir, $source);
+	opendir (my $dir, $source);
 	my $finalSource = "";
 	my $finalTimestamp = "";
 	my $tmpTimestamp = "0000_00_00_00_00_00";
-	while(readdir $dir)
-	{
-		if($_ ne "." and $_ ne ".." and $_ ne ".DS_Store")
-		{
+	while (readdir $dir) {
+		if ($_ ne "." and $_ ne ".." and $_ ne ".DS_Store") {
 			my ($archiveName, $archiveTimestamp) = split(/_/, $_, 2);
 			$self->{verbosity}->verbose("Compare times:");
-			if($archiveName)
-			{
-				if($self->compare_to($archiveTimestamp) <= $self->compare_to($timestamp))
-				{
+			if ($archiveName) {
+				if ($self->compare_to($archiveTimestamp) <= $self->compare_to($timestamp)) {
 					$self->{verbosity}->verbose("Compare $archiveTimestamp with $timestamp");
-					if($self->compare_to($archiveTimestamp) > $self->compare_to($tmpTimestamp))
-					{
+					if ($self->compare_to($archiveTimestamp) > $self->compare_to($tmpTimestamp)) {
 						$tmpTimestamp = $archiveTimestamp;
 						$finalSource = "${source}/${_}";
 						$finalTimestamp = $archiveTimestamp;
@@ -46,20 +53,18 @@ sub findLastValidArchive {
 			}
 		}
 	}
-	closedir($dir);
+	closedir ($dir);
 	return $finalSource;
 };
 
+# compare_to
+# Beschreibung: Berechnet einen int-Wert aus einem Datum
+# Parameter:	date	Datum
 sub compare_to {
 	my ($self, $date) = @_;
 	my($y,$m,$d,$H,$M,$S) = split(/_/,$date);
 	return "$y$m$d$H$M$S";
 };
-
-sub get_os {
-	my $self = shift;
-	return $^O;
-}
 
 1;
 __END__
