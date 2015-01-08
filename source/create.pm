5ba10a927ab5e91e5da4d292a3a417bb73955cf6
Ramunno <RamunnoMi53355@th-nuernberg.de>
2015-01-08 02:06:01 +0100

    Create wurde angepasst.
    Windows links erstellen und aktualisieren funktioniert jetzt.
    Weitere Änderungen.

--- source/create.pm
+++ source/create.pm
@@ -169,9 +169,79 @@ sub verbose {
         # Ausgabe der Nachricht
         use Verbosity;
         my $v = Verbosity->new(1);
+        if($^O eq "MSWin32")
+        {
+            $_=$message;
+            s/\//\\/g;
+            $message=$_;
+        }
         $v->verbose($message,$state);
     }
 }
+
+#*****************************************************************************************************
+#                                        copyFile
+# Beschreibung: Kopiert eine Datei Datei
+# Parameter:    $self = Instanz von Create
+#               $fileName = Name der Datei die gelöscht werden soll
+#               $fileSource = Verzeichnis in dem sich die Datei befindet
+#               $fileDestination = Verzeichnis in das die Datei kopiert wereden soll
+#*****************************************************************************************************
+my $copyFile = sub{
+    my ($self,$fileName,$fileSource,$fileDestination)=@_;
+    # Ausgabe auf der Konsole
+    if($^O eq "MSWin32")
+    {
+        $self->verbose("Copy file:\nSource:\t\t$fileSource\\$fileName\nDestination:\t$fileDestination\\$fileName");
+    }
+    else
+    {
+        $self->verbose("Copy file:\nSource:\t\t\t$fileSource/$fileName\nDestination:\t$fileDestination/$fileName");
+    }
+    # Kopieren der Datei
+    if(copy("$fileSource/$fileName","$fileDestination/$fileName"))
+    {
+        # Datei wurde kopiert
+        $self->verbose("Copied!\n","OK");
+    }
+    else
+    {
+        # Datei konnte nicht kopiert werden
+        $self->verbose("Can't copy file $fileName!\n","ERROR");
+    }
+};
+
+#*****************************************************************************************************
+#                                        createDir
+# Beschreibung: Erzeugt einen neuen Verzeichnisses
+# Parameter:    $self = Instanz von Create
+#               $dirName = Name des Verzeichnisses
+#               $dirPath = Path des Verzeichnisses
+#*****************************************************************************************************
+my $createDir = sub{
+    my ($self,$dirName,$dirPath)=@_;
+    # Ausgabe auf der Konsole
+    if($^O eq "MSWin32")
+    {
+        $self->verbose("Create Directroy:\nDestination:\t$dirPath\\$dirName");
+    }
+    else
+    {
+        $self->verbose("Create Directroy:\nDestination:\t$dirPath/$dirName");
+    }
+    # Verzeichnis erstellen
+    if(mkdir("$dirPath/$dirName"))
+    {
+        # Verzeichnis wurde ertellt
+        $self->verbose("Created!\n","OK");
+    }
+    else
+    {
+        # Verzeichnis konnte nicht erstellt werden
+        $self->verbose("Can't create directory $dirName!\n","ERROR");
+    }
+
+};
 
 #*****************************************************************************************************
 #                                         copyDir
@@ -183,33 +253,168 @@ sub copyDir {
     my ($self,$directory,$destination)=@_;
     # Öffnen des Quellverzeichnisses bzw. dessen Unterverzeichnisses
     opendir(my $dsh,"$self->{source}/$directory") || die("Can't find directory $directory: $!");
+    $self->verbose("Open directory: $self->{source}$directory\n","OK");
     # Kopieren aller Dateien bzw. Erzeugen eines Unterverzeichnisses
-    while(readdir $dsh) {
-        if($_ ne ".." and $_ ne ".")
+    while(my $file=readdir $dsh) {
+        if($file ne ".." and $file ne ".")
         {
             # Überprüfen ob es sich um eine Datei handelt
-            if (-f "$self->{source}/$directory/$_")
+            if (-f "$self->{source}/$directory/$file")
             {
-                $self->verbose("Copy file:\tSource = $self->{source}$directory/$_\n\t\t\tDestination = $self->{destination}/$destination$directory/$_\n","OK");
                 # Kopieren der Datei
-                copy("$self->{source}/$directory/$_","$self->{destination}/$destination/$directory/$_")or die "Can't copy file $_ : $!";
+                $copyFile->($self,"$file","$self->{source}$directory","$self->{destination}/$destination$directory");
             }
             # Überprüfen ob es sich um ein Verzeichnis handelt
-            elsif (-d "$self->{source}/$directory/$_")
+            elsif (-d "$self->{source}/$directory/$file")
             {
-                $self->verbose("Create directory:\t$self->{destination}/$destination$directory/$_\n","OK");
                 # Erzeugen des Unterverzeichnisses
-                mkdir("$self->{destination}/$destination$directory/$_");
-                $self->verbose("Change directory:\t$self->{source}$directory/$_\n","OK");
+                $createDir->($self,"$file","$self->{destination}/$destination$directory");
                 # Rekursiver Aufruf der Methode zum Kopieren des Unterverzeichnisses
-                $self->copyDir("$directory/$_","$destination");
+                $self->copyDir("$directory/$file","$destination");
             }
         }
     }
-    $self->verbose("Leave directory:\t$self->{source}$directory\n","OK");
     # Schließen des Quellverzeichnisses bzw. dessen Unterverzeichnisses
     closedir($dsh);
+    $self->verbose("Leave directory: $self->{source}$directory\n","OK");
 }
+
+#*****************************************************************************************************
+#                                        deleteFile
+# Beschreibung: Erzeugt einen neuen symbolischen Link auf eine Datei
+# Parameter:    $self = Instanz von Create
+#               $fileName = Name der Datei die gelöscht werden soll
+#               $filePath = Verzeichnis in dem sich die Datei befindet
+#*****************************************************************************************************
+my $deleteFile = sub{
+    my ($self,$fileName,$filePath)=@_;
+    # Ausgabe auf der Konsole
+    if($^O eq "MSWin32")
+    {
+        $self->verbose("Delete file:\n$filePath\\$fileName");
+    }
+    else
+    {
+        $self->verbose("Delete file:\n$filePath/$fileName");
+    }
+    # Löschen der alten Datei
+    if(unlink("$filePath/$fileName")>0)
+    {
+        # Datei wurde gelöscht
+        $self->verbose("Deleted!\n","OK");
+    }
+    else
+    {
+        # Datei konnte nicht gelöscht werden
+        $self->verbose("Can't delete file $fileName!\n","ERROR");
+    }
+};
+
+#*****************************************************************************************************
+#                                        createLink
+# Beschreibung: Erzeugt einen neuen symbolischen Link auf eine Datei
+# Parameter:    $self = Instanz von Create
+#               $fileName = Name der Datei die verlinkt werden soll
+#               $linkSource = Verzeichnis in dem sich die originale Datei befindet
+#               $linkDestination = Verzeichnis in dem der Link erstellt werden soll
+#*****************************************************************************************************
+my $createLink = sub{
+    my ($self,$fileName,$linkSource,$linkDestination)=@_;
+    # Überprüfen um welches Betriebssystem es sich handelt
+    my $result="false";
+    if($^O eq "MSWin32")
+    {
+        # Falls Windows, erzeugen eines shortcuts
+        $self->verbose("Link files:\nOriginal:\t$linkDestination\\$fileName\nLink:\t\t$linkSource\\$fileName");
+        require Win32::Shortcut or die("Can't import Win32::Shortcut: $!");
+        my $link = Win32::Shortcut->new();
+        $link->{'Path'}="$linkSource\\$fileName";
+        $link->{'File'}="$linkDestination\\$fileName.lnk";
+        $result=$link->Save();
+    }
+    else
+    {
+        # Falls Unix basiertes Betriebssystem, erzeugen eines softlinks
+        $self->verbose("Link files:\nOriginal:\t$linkDestination/$fileName\nLink:\t$linkSource/$fileName");
+        $result=symlink("$linkSource/$fileName","$linkDestination/$fileName");
+    }
+    if($result)
+    {
+        # Datei wurde verlinkt
+        $self->verbose("Linked!\n","OK");
+    }
+    else
+    {
+        # Datei konnte nicht verlinkt werden
+        $self->verbose("Can't create link!\n","ERROR");
+    }
+};
+
+#*****************************************************************************************************
+#                                        updateLink
+# Beschreibung: Erzeugt einen neuen symbolischen Link auf eine Datei
+# Parameter:    $self = Instanz von Create
+#               $linkName = Name der Linkdatei
+#               $linkSource = Verzeichnis in dem sich die originale Datei befindet
+#               $linkPath = Verzeichnis in dem sich der Link befindet
+#*****************************************************************************************************
+my $updateLink = sub{
+    my ($self,$linkName,$linkSource,$linkPath)=@_;
+    my $result="false";
+    if($^O eq "MSWin32")
+    {
+        require Win32::Shortcut or die("Can't import Win32::Shortcut: $!");
+        my $link = Win32::Shortcut->new();
+        $link->Load("$linkPath\\$linkName");
+        $self->verbose("Update link:\nOld:\t$link->{'Path'}\nNew:\t$linkSource");
+        $link->{'Path'}="$linkSource";
+        $result=$link->Save();
+        $link->Close();
+        if($result)
+        {
+            # Verlinkung wurde aktualisiert
+            $self->verbose("Link updated!\n","OK");
+        }
+        else
+        {
+            # Verlinkung konnte nicht aktualisiert
+            $self->verbose("Can't update link!\n","ERROR");
+        }
+    }
+};
+
+#*****************************************************************************************************
+#                                        getLinkPath
+# Beschreibung: Überpruft ob link gültig ist
+# Parameter:    $self = Instanz von Create
+#               $linkName = Name der Linkdatei
+#               $linkPath = Verzeichnis in dem sich der Link befindet
+#*****************************************************************************************************
+my $getLinkPath = sub{
+    my ($self,$linkName,$linkPath)=@_;
+    my $result=0;
+    my $path="";
+    if($^O eq "MSWin32")
+    {
+        require Win32::Shortcut or die("Can't import Win32::Shortcut: $!");
+        my $link = Win32::Shortcut->new();
+        $result=$link->Load("$linkPath\\$linkName");
+        $self->verbose("Get link Path:\n$linkPath\\$linkName");
+        $path=$link->{'Path'};
+        $link->Close();
+        if($result)
+        {
+            # Verlinkung wurde aktualisiert
+            $self->verbose("Link found!\n","OK");
+        }
+        else
+        {
+            # Verlinkung konnte nicht aktualisiert
+            $self->verbose("Can't find link!\n","ERROR");
+        }
+    }
+    return $path;
+};
 
 #*****************************************************************************************************
 #                                        compareDir
@@ -221,52 +426,63 @@ sub copyDir {
 sub compareDir {
     my ($self,$olderDir,$newerDir)=@_;
     # Öffnen des Archivverzeichnisses bzw. dessen Unterverzeichnisse
-    opendir(my $doh,"$self->{destination}/$olderDir") || die("Can't find directory $olderDir: $!");
+    opendir(my $doh,"$self->{destination}/$newerDir") || die("Can't find directory $newerDir: $!");
     # Verglichen aller Dateien, inklusive Unterverzeichnisse
-    while(my $oldFile=readdir $doh ) {
-        if($oldFile ne ".." and $oldFile ne ".")
+    while(my $newFile=readdir $doh) {
+        if($newFile ne ".." and $newFile ne ".")
         {
             # Überprüfen ob es sich um eine Datei handelt
-            if (-f "$self->{destination}/$olderDir/$oldFile" )
+            if (!-d "$self->{destination}/$newerDir/$newFile")
             {
-                $self->verbose("Compare File:\n$self->{destination}/$olderDir/$oldFile\n$self->{destination}/$newerDir/$oldFile","OK");
+                # Entfernen von lnk-Endung
+                $_=$newFile;
+                s/.lnk//;
+                my $newFileNoLink=$_;
                 # Überprüfung der Dateien auf Gleichheit
-                my $result=$self->compareFile("$self->{destination}/$olderDir/$oldFile","$self->{destination}/$newerDir/$oldFile");
-                if($result eq "true")
+                if($^O eq "MSWin32" and $newFile=~/.lnk$/i)
                 {
-                    # Falls beide Dateien gleich sind
-                    $self->verbose("Delete file:\n$self->{destination}/$olderDir/$oldFile\n","OK");
-                    # Löschen der alten Datei
-                    unlink("$self->{destination}/$olderDir/$oldFile");
-                    $self->verbose("Link file:\n$self->{destination}/$olderDir/$oldFile\n=> $self->{destination}/$newerDir/$oldFile\n","OK");
-                    # Überprüfen um welches Betriebssystem es sich handelt
-                    if($^O eq "MSWin32")
+                    # Überprüfe ob alte Datei link ist
+                    if(-e "$self->{destination}\\$olderDir\\$newFile")
                     {
-                        # Falls Windows, erzeugen eines shortcuts
-                        require Win32::Shortcut or die("Can't import Win32::Shortcut: $!");
-                        my $link = Win32::Shortcut->new();
-                        $link->{'Path'}="$self->{destination}/$newerDir/$oldFile";
-                        my ($fileName)=split('\.',$oldFile);
-                        $fileName=$fileName.".lnk";
-                        $link->{'File'}="$self->{destination}/$olderDir/$fileName";
-                        $link->Save();
-                        $link->Close();
+                        my $path=$getLinkPath->($self,"$newFile","$self->{destination}\\$newerDir");
+                        $updateLink->($self,$newFile,$path,"$self->{destination}\\$olderDir");
+                        next;
+                    }
+                    # Überpüfe ob alte Datei als normale Datei vorhanden ist
+                    if(-e "$self->{destination}\\$olderDir\\$newFileNoLink")
+                    {
+                        # Hole Pfad der aktuellen Datei
+                        $_=$getLinkPath->($self,"$newFile","$self->{destination}\\$newerDir");
+                        # Entfernen des Dateinames
+                        s/\\$newFileNoLink//;
+                        my $path=$_;
+                        # Falls beide Dateien gleich sind
+                        $deleteFile->($self,$newFileNoLink,"$self->{destination}/$olderDir");
+                        # Erstellen eines Links zur neueren Datei
+                        $createLink->($self,$newFileNoLink,$path,"$self->{destination}/$olderDir");
+                        next;
                     }
                     else
                     {
-                        # Falls Unix basiertes Betriebssystem, erzeugen eines softlinks
-                        symlink("$self->{destination}/$newerDir/$oldFile","$self->{destination}/$olderDir/$oldFile");
+                        next;
                     }
+                }
+                my $result=$self->compareFile("$self->{destination}/$olderDir/$newFileNoLink","$self->{destination}/$newerDir/$newFile");
+                if($result eq "true")
+                {
+                    # Falls beide Dateien gleich sind
+                    $deleteFile->($self,$newFile,"$self->{destination}/$olderDir");
+                    # Erstellen eines Links zur neueren Datei
+                    $createLink->($self,$newFile,"$self->{destination}/$newerDir","$self->{destination}/$olderDir");
                 }
             }
             # Überprüfen ob es sich um ein Verzeichnis handelt
-            elsif (-d "$self->{destination}/$olderDir/$oldFile")
+            elsif (-d "$self->{destination}/$olderDir/$newFile")
             {
-                $self->verbose("Compare Diretory:\n$self->{destination}/$olderDir/$oldFile\n$self->{destination}/$newerDir/$oldFile\n","OK");
+                $self->verbose("Compare Diretory:\n$self->{destination}/$olderDir/$newFile\n$self->{destination}/$newerDir/$newFile\n");
                 # Rekursiver Aufruf der Methode zur Untesuchung des Unterverzeichnisses
-                $self->compareDir("$olderDir/$oldFile","$newerDir/$oldFile");
+                $self->compareDir("$olderDir/$newFile","$newerDir/$newFile");
             }
-            
         }
     }
     # Schließen des Archivverzeichnisses bzw. dessen Unterverzeichnisses
@@ -278,19 +494,21 @@ sub compareDir {
 # Beschreibung: Vergleicht zwei Dateien aus unterschiedlichen Archiven auf Gleichheit
 # Parameter:    olderFile = ältere Datei
 #               newerFile = neuere Datei
+# Return:       "true"  -> Files are equal
+#               "false" -> Files are diffrent
 #*****************************************************************************************************
 sub compareFile {
     my ($self,$olderFile,$newerFile)=@_;
+    # Ausgabe auf Konsole
+    $self->verbose("Compare Files:\nOld file:\t$olderFile\nNew file:\t$newerFile");
     # Importieren des Core Module Digest
     use Digest;
     # Erzeugen eines MD5 Objekts
     my $md5=Digest->MD5;
     # Öffnen der älteren Datei zum Lesen
-    open(my $foh, "<", "$olderFile")
-    or return "false";
+    open(my $foh, "<", "$olderFile") or return "false";
     # Öffnen der neueren Datei zum Lesen
-    open(my $fnh, "<", "$newerFile")
-    or return "false";
+    open(my $fnh, "<", "$newerFile") or return "false";
     # Hinzufügen der älteren Datei
     $md5->addfile($foh);
     # Erzeugen des Hashwerts für die ältere Datei
@@ -307,7 +525,7 @@ sub compareFile {
     if($digestOldFile eq $digestNewFile)
     {
         # Falls beide Dateien gleich sind => unverändert
-        $self->verbose("=> Files are equal!\n","OK");
+        $self->verbose("Files are equal!\n","OK");
         return "true";
     }
     else
@@ -315,12 +533,12 @@ sub compareFile {
         if(-l $olderFile or $olderFile=~/.lnk$/)
         {
             # Falls ältere Datei bereits ein link ist => unverändert
-            $self->verbose("=> Old file is already link!\n","OK");
+            $self->verbose("Old file is already link!\n","OK");
             return "true";
             
         }
         # Falls beide Dateien nicht gleich sind => verändert
-        $self->verbose("=> Files are diffrent!\n","OK");
+        $self->verbose("Files are diffrent!\n","OK");
         return "false";
     }
 }
