3fdc4f34fd886f6698af0d297fc82a0cfbfc7de2
KasikciMu <muhammedkasikci@Muhammeds-MacBook-Air.local>
2015-01-17 00:06:41 +0100

    Some Changes

--- source/RestoreWin.pm
+++ source/RestoreWin.pm
@@ -9,7 +9,6 @@ use strict;
 use warnings;
 use File::Find;
 use File::Copy;
-use File::Copy::Recursive;
 use File::Path qw(make_path remove_tree);
 use Verbosity;
 use Utils;
@@ -17,7 +16,7 @@ use Utils;
 
 package RestoreWin;
 
-our $FinalTime;
+our $Flag = "0";
 
 sub new
 {
@@ -39,17 +38,30 @@ sub new
 
 sub addSource{
     my ($self,$Source) = @_;
-    $self->{source}=$Source;
-    $self->{verbosity}->verbose("New source added: $self->{source}");
+    if(-e $Source){
+        $self->{source}=$Source;
+        $self->{verbosity}->verbose("New source added: $self->{source}");
+    }
+    else
+    {
+        $self->{verbosity}->verbose("Can't find Source: $Source","ERROR");
+    }
+    
 }
 sub addDestination{
     my ($self, $Destination) = @_;
-    $self->{destination} = $Destination;
-    $self->{verbosity}->verbose("New destination added: $self->{destination}");
+    if(-e $Destination){
+        $self->{destination}=$Destination;
+        $self->{verbosity}->verbose("New destination added: $self->{destination}");
+    }
+    else
+    {
+        $self->{verbosity}->verbose("Can't find Source: $Destination","ERROR");
+    }
 }
 sub addSourceName{
     my ($self, $Sourcename) = @_;
-    $self->{sourcename} = $Sourcename;
+    $self->{sourcename}= $Sourcename;
     $self->{verbosity}->verbose("New source name added: $self->{sourcename}");
 }
 sub addUserTime{
@@ -62,7 +74,7 @@ sub addPartial{
     my($self,$Partial) = @_;
     $self->{partial} = $Partial;
     $self->{rel_path} = "0";
-    $self->{rel_path} = "1" if($Partial =~ m/\//);
+    $self->{rel_path} = "1" if($Partial =~ m/\\/);
     $self->{verbosity}->verbose("New partial added: $self->{partial}");
 }
 sub setVerboseLevel{
@@ -70,40 +82,27 @@ sub setVerboseLevel{
     $self->{verbosity}->setVerboseLevel($level);
 }
 
-sub restore_r
-{
+sub restore_r{
     my $self = shift;
     $self->{verbosity}->verbose("Start Restore r.\n","OK");
-    my $tmp = Utils->new();
-    $self->{verbosity}->verbose("Call findLastValidArchive\n","OK");
-    my $SourceArchiv = $tmp->findLastValidArchive
-    (
-    $self->{source},
-    $self->{usertime},
-    );
+    $self->{verbosity}->verbose("Call FindArchive\n","OK");
+    my $SourceArchiv = $self->FindArchive();
     $self->{verbosity}->verbose("Find Source Directory: $SourceArchiv.\n","OK");
     $self->{verbosity}->verbose("The given path is not a directory!\n","ERROR") if(!(-d $SourceArchiv));
     die if(!(-d $SourceArchiv));
-    $self->verbosity->verbose("Call Restore Directory!\n","OK");
-    RestoreDirectory
+    $self->{verbosity}->verbose("Call Restore Directory!\n","OK");
+    $self->RestoreDirectory
     (
     $SourceArchiv,
-    $self,
     );
 }
 
-sub restore_rp
-{
+sub restore_rp{
     my $self = shift;
     $self->{verbosity}->verbose("Start Restore rp.","OK");
-    my $tmp = Utils->new();
     # Hier wird der genaue Pfad (d.h. mit datetime Ergänzung herausgefunden)
-    $self->{verbosity}->verbose("Call findLastValidArchive\n","OK");
-    my $SourceArchiv = $tmp->findLastValidArchive
-    (
-    $self->{source},
-    $self->{usertime},
-    );
+    $self->{verbosity}->verbose("Call FindArchive\n","OK");
+    my $SourceArchiv = $self->FindArchive();
     # Falls ein relativer Pfad angegeben wurde werden die angegeben Pfade um den relative Pfad erweitert
     my $partial = $self->{partial};
     my $FileorArchivSource = "";
@@ -112,12 +111,12 @@ sub restore_rp
     if($self->{rel_path} eq "1"){
         $self->{verbosity}->verbose("Partial is a relative path\n","OK");
         $FileorArchivSource = "$SourceArchiv$self->{partial}";
-        my @tmp2 = split(/\//,$SourceArchiv);
+        my @tmp2 = split(/\\/,$SourceArchiv);
         my $tmp2 = scalar(@tmp2)-1;
         my $tmp3 = $tmp2[$tmp2];
         my @split1 = split(/_\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}$/, $tmp3);
         my $archiveName = $split1[0];
-        $FileorArchivDestination = "$self->{destination}/$archiveName$self->{partial}";
+        $FileorArchivDestination = "$self->{destination}\\$archiveName$self->{partial}";
     }
     else{
         # Falls dies nicht der Fall ist muss die Datei oder das Unterverzeichnis gesucht werden dabei wird der erste Treffer ausgewertet
@@ -127,7 +126,6 @@ sub restore_rp
         (
         $SourceArchiv,
         $partial,
-        $self,
         );
         $self->{verbosity}->verbose("The given source subdirectory or file does not exist!\n","ERROR") if(!(-f $FileorArchivSource || -d $FileorArchivSource));
         die if (!(-f $FileorArchivSource || -d $FileorArchivSource));
@@ -137,7 +135,6 @@ sub restore_rp
         (
         $destination,
         $partial,
-        $self,
         );
         $self->{verbosity}->verbose("The given destination subdirectory or file does not exist!\n","ERROR") if(!(-f $FileorArchivSource || -d $FileorArchivSource));
     }
@@ -161,7 +158,6 @@ sub restore_rp
         (
         $FileorArchivSource,
         $FileorArchivDestination,
-        $self,
         );
     }
     elsif(-f $FileorArchivSource)
@@ -175,7 +171,6 @@ sub restore_rp
             (
             $FileorArchivSource,
             $FileorArchivDestination,
-            $self,
             );
         }
     }
@@ -186,25 +181,24 @@ sub restore_rp
 #									Find_source_rp										 #
 #				Hilfsfunktion um eine bestimmte Datei oder Verzeichnis zu finden		 #
 ##########################################################################################
-sub Find_source_rp
-{
-    my ($Directory,$partial,$self) = @_;
+sub Find_source_rp{
+    my ($self,$Directory,$partial) = @_;
     $self->{verbosity}->verbose("Open directory $Directory\n","OK");
     opendir Dir, $Directory || die "Can`t open Directory!";
     my @Files = readdir Dir;
     closedir Dir;
     $self->{verbosity}->verbose("Close directory $Directory\n","OK");
-    return "${Directory}/${partial}" if(grep(/$partial/,@Files));
+    return "${Directory}\\${partial}" if(grep(/$partial/,@Files));
     foreach (@Files)
     {
         if($_ ne "." and $_ ne ".." and $_ ne ".DS_Store")
         {
-            if(-d "${Directory}/${_}")
+            if(-d "${Directory}\\${_}")
             {
                 $self->{verbosity}->verbose("Found partial\n","OK"), if($_ eq $partial);
-                return "${Directory}/${_}" if($_ eq $partial);
+                return "${Directory}\\${_}" if($_ eq $partial);
                 $self->{verbosity}->verbose("Call Find_source_rp");
-                return Find_source_rp("${Directory}/${_}",$partial);
+                return Find_source_rp("${Directory}\\${_}",$partial);
             }
         }
     }
@@ -216,31 +210,70 @@ sub Find_source_rp
 #				Hilfsfunktion um ein ganzes Verzeichnis wieder herzustellen				 #
 ##########################################################################################
 sub RestoreDirectory{
-    my($SourceArchiv,$self) = @_;
-    my $DestinationName = DestinationArchiv($self->{destination},$self->{sourcename});
-    $self->{verbosity}->verbose("Remove $self->{destination}/${DestinationName}\n","OK") if(-d "$self->{destination}/${DestinationName}");
-    File::Path::remove_tree("$self->{destination}/${DestinationName}") if(-d "$self->{destination}/${DestinationName}");
-    $self->{verbosity}->verbose("Make $self->{destination}/$self->{sourcename}}\n","OK");
-    File::Path::make_path("$self->{destination}/$self->{sourcename}");
-    $self->{verbosity}->verbose("Copy from $SourceArchiv to $self->{destination}}/$self->{sourcename}","OK");
-    File::Copy::Recursive::dircopy($SourceArchiv,"$self->{destination}/$self->{sourcename}");
+    my($self,$SourceArchiv) = @_;
+    # Falls es Probleme während dem Kopiervorgang entstehen sollten wird der alte Ordner umbennant und bei einem Fehler der neu erstellt Ordner gelöscht
+    my @tmp = split (/\\/,$self->{destination});
+    my $tmpdestination = "$tmp[0]";
+    for(my $i=1; $i<=($#tmp-1);$i++)
+    {
+        $tmpdestination .= "\\$tmp[$i]";
+    }
+    my $NewDest = $tmpdestination."\\$tmp[$#tmp] 2";
+    $self->{verbosity}->verbose("Rename $self->{destination} to $NewDest!","OK");
+    rename $self->{destination}, $NewDest;
+    
+    $self->{verbosity}->verbose("Copy from $SourceArchiv to $self->{destination}}","OK");
+    $self->RecusriveRestore($SourceArchiv,"$self->{destination}");
+    
+    if($Flag eq "0")
+    {
+        $self->{verbosity}->verbose("Remove $NewDest","OK") if(-d "$NewDest");
+        File::Path::remove_tree("$NewDest")if(-d "$NewDest");
+    }
+    else
+    {
+        $self->{verbosity}->verbose("Copy failed!","ERROR");
+        $self->{verbosity}->verbose("Remove $self->{destination}","WARNING");
+        $self->{verbosity}->verbose("Rename $NewDest to $self->{destination}","WARNING");
+        File::Path::remove_tree("$self->{destination}");
+        rename $NewDest, $self->{destination};
+    }
 }
 
 
-
 ##########################################################################################
-#									DestinationArchiv									 #
-#				Hilfsfunktion um die genau Adresse des Zielverzeichnis zu erhalten		 #
+#									RecusriveRestore									 #
+#				Hilfsfunktion um ein Verzeichnis wieder herzustellen					 #
 ##########################################################################################
-sub DestinationArchiv{
-    my ($Destination,$SourceName) = @_;
-    opendir DIR,$Destination;
-    my @DestinationFiles = readdir DIR;
-    closedir DIR;
-    foreach (@DestinationFiles)
+sub RecusriveRestore{
+    my ($self, $Source,$Destination) = @_;
+    $self->{verbosity}->verbose("Make $self->{destination}\n","OK");
+    mkdir $Destination;
+    opendir my ($Dir), $Source;
+    my @Files = readdir $Dir;
+    closedir $Dir;
+    foreach (@Files)
     {
-        if ($_ =~ m/${SourceName}*/){
-            return $_;
+        if($_ ne ".." and $_ ne "."){
+            if (-l "$Source\\$_" or $_ =~ m/.lnk$/i)
+            {
+                print "YES a Link!\n";
+                my $SourceDatei = $self->getLinkPath($_,$Source);
+                $self->RestoreFile($SourceDatei,$Destination,$_);
+            }
+            elsif(-f "$Source\\$_"){
+                print "YES a file!\n";
+                $self->RestoreFile("$Source\\$_",$Destination, $_);
+            }
+            elsif(-d "$Source\\$_")
+            {
+                mkdir("$Destination\\$_");
+                $self->RecusriveRestore("$Source\\$_","$Destination\\$_");
+            }
+            else{
+                $self->{verbosity}->verbose("This is not a file or a directory $Source\\$_");
+                $Flag = "1";
+            }
         }
     }
 }
@@ -251,13 +284,13 @@ sub DestinationArchiv{
 #				Hilfsfunktion um ein Unterverzeichnis wieder herzustellen				 #
 ##########################################################################################
 sub RestoreSubDirectory{
-    my($Source,$Destination,$self)=@_;
+    my($self,$Source,$Destination)=@_;
     $self->{verbosity}->verbose("Remove $Destination\n","OK")if(-d $Destination);
     File::Path::remove_tree($Destination) if(-d $Destination);
     $self->{verbosity}->verbose("Make Dir: ${Destination}\n","OK");
-    File::Path::make_path("$Destination");
-    $self->{verbosity}->verbose("Copy Dir from $Source to ${Destination}/$self->{partial}\n","OK");
-    File::Copy::Recursive::dircopy($Source, $Destination);
+    mkdir("$Destination");
+    $self->{verbosity}->verbose("Copy Dir from $Source to ${Destination}\n","OK");
+    RecusriveRestore($Source,$Destination);
 }
 
 
@@ -266,26 +299,82 @@ sub RestoreSubDirectory{
 #				Hilfsfunktion um eine Datei wieder herzustellen							 #
 ##########################################################################################
 sub RestoreFile{
-    my($Source,$Destination,$self)=@_;
-    if(-f $Destination)
-    {
-        $self->{verbosity}->verbose("Delete Destinationfile: $Destination.\n");
-        unlink $Destination or die "Konnte nicht gelöscht werden!";
-        $self->{verbosity}->verbose("Copy File to $Destination\n");
-        File::Copy::copy $Source,$Destination;
+    my($self,$SourceFile,$Destination,$File)=@_;
+    if(-e "$Destination\\$File"){
+        $self->{verbosity}->verbose("Delete Destinationfile: $Destination.\n","OK");
+        unlink "$Destination\\$File" or die "Can`t delete File $Destination!";
+        $self->{verbosity}->verbose("Copy File $SourceFile to $Destination\n","OK");
+        File::Copy::copy $SourceFile,$Destination;
     }
-    elsif(-d $Destination)
-    {
-        $self->{verbosity}->verbose("Copy file to $Destination","OK");
-        File::Copy::copy($Source,$Destination);
+    else{
+        $self->{verbosity}->verbose("Copy File $SourceFile to $Destination\n","OK");
+        File::Copy::copy $SourceFile,$Destination;
     }
+    
 }
 
 
+##########################################################################################
+#									FindArchive                                          #
+#				Hilfsfunktion um das zu wiederherstellende Archvi finden				 #
+##########################################################################################
+sub FindArchive{
+    my $self = shift;
+    my $hash = "";
+    open (TXT, "$self->{source}\\hashtable.txt") or die "Can not open hashtable file!";
+    while(<TXT>)
+    {
+        if($_ =~ m/$self->{sourcename}$/)
+        {
+            my @tmp = split(/:/,$_);
+            $hash = $tmp[0];
+            $self->{verbosity}->verbose("Find Hash: $hash","OK");
+        }
+    }
+    if($hash eq ""){
+        $self->{verbosity}->verbose("Can't find Archiv with the same name!","ERROR");
+        die;
+    }
+    use Utils;
+    $tmp = Utils->new();
+    $self->{verbosity}->verbose("Call findLastValidArchive","OK");
+    return $tmp->findLastValidArchive($self->{source}, $self->{usertime},$hash);
+}
+
+##########################################################################################
+#									getLinkPath											 #
+#				Hilfsfunktion um die orginal Datei zu finden							 #
+##########################################################################################
+sub getLinkPath{
+    my ($self,$linkName,$linkPath)=@_;
+    my $result=0;
+    my $path="";
+    if($^O eq "MSWin32")
+    {
+        require Win32::Shortcut or die("Can't import Win32::Shortcut: $!");
+        my $link = Win32::Shortcut->new();
+        $result=$link->Load("$linkPath\\$linkName");
+        $self->{verbosity}->verbose("Get original file path from link:\nLink:\t$linkPath\\$linkName\nPath:\t$link->{'Path'}","OK");
+        $path=$link->{'Path'};
+        $link->Close();
+        if($result)
+        {
+            # Verlinkung wurde aktualisiert
+            $self->{verbosity}->verbose("Link found!\n","OK");
+        }
+        else
+        {
+            # Verlinkung konnte nicht aktualisiert
+            $self->{verbosity}->verbose("Can't find link!\n","ERROR");
+        }
+    }
+    return $path;
+};
+
 sub DESTROY{
     my $self = shift;
-    $FinalTime = undef;
     $self->{verbosity}=undef;
+    $flag = undef;
 };
 1;
 __END__
