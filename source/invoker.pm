f3cd26cc68d28d69e6aa005701eefaf2fd78aa92
Patrick Vogt <patrick.vogt@hs-augsburg.de>
2015-01-18 00:02:42 +0100

    improve invoker

--- source/invoker.pm
+++ source/invoker.pm
@@ -71,7 +71,7 @@ sub slim {
 			@split2 = split(/\//, $split[0]);
 		}
 		my @split3 = split(/$split2[$#split2]\//, $arguments[0]);
-		$create->addDestination($split3[0]);
+		$create->addDestination(Cwd::abs_path($split3[0]));
 		$create->addArchiveName($split2[$#split2]);
 		$create->create_s();
 	} else {
