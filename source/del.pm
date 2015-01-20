3f2f6861889cf643a0636d09dc1745b8436a44e6
mia8287 <mia8287@gmail.com>
2015-01-20 18:45:06 +0100

    Kommentare entfernt

--- source/del.pm
+++ source/del.pm
@@ -1,9 +1,6 @@
 package del;
 use strict;
 use warnings;
-#use File::Basename qw(dirname);
-#use Cwd qw(abs_path);
-#use lib qw(dirname(dirname abs_path $0) . "/PerlArchiver/source");
 use Win32::Shortcut;
 use File::Find;
 use File::Copy;
